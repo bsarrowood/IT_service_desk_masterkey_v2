e3bcb0dc841a2f550436cda9d629591bc5acff49
Brad Arrowood <61139407+reurbo@users.noreply.github.com>
2020-08-09 19:37:15 -0500

    Update IT_service_desk_masterkey_v2.ps1

--- IT_service_desk_masterkey_v2.ps1
+++ IT_service_desk_masterkey_v2.ps1
@@ -1,21 +1,21 @@
-# Created by:		reurbo
+# Created by:		Brad Arrowood
 # Created on:		2019.06.01
-# Last updated:    	2019.10.29
+# Last updated:		2019.10.29
 # Script name:		IT_service_desk_masterkey_v2.ps1 (aka slim.ps1)
 # Description:		An updated script from my previous IT_service_desk_masterkey_v1.bat to include
-#					new features and potentially speed up previously slower tasks from the last version.
-#					Some features require nircmd.exe (listed in the References) to do certain tasks.
-#					I have the file set to be copied from a specific folder to the target computer for
-#					those tasks, run what is needed, then remove the file to clean up after itself.
+#			new features and potentially speed up previously slower tasks from the last version.
+#			Some features require nircmd.exe (listed in the References) to do certain tasks.
+#			I have the file set to be copied from a specific folder to the target computer for
+#			those tasks, run what is needed, then remove the file to clean up after itself.
 
-# NOTICE:			I use PowerShell ISE for this and you can hide the script pane using Ctrl+R.
-#					Be aware, if you are getting an error you aren't allowed to run scripts in PowerShell
-#					you'll need to do the following:
-#						1. Open an Administrator instance of PowerShell ISE
-#						2. Enter the following: Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-#						3. When prompted, choose "Yes to All"
-#					You can then close out of the Administrator PowerShell ISE, open a regular 
-#					PowerShell ISE, open the script and run it.
+# NOTICE:		I use PowerShell ISE for this and you can hide the script pane using Ctrl+R.
+#			Be aware, if you are getting an error you aren't allowed to run scripts in PowerShell
+#			you'll need to do the following:
+#				1. Open an Administrator instance of PowerShell ISE
+#				2. Enter the following: Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+#				3. When prompted, choose "Yes to All"
+#			You can then close out of the Administrator PowerShell ISE, open a regular 
+#			PowerShell ISE, open the script and run it.
 
 # References:
 #	https://techtalk.gfi.com/11-most-useful-powershell-commands-for-remote-management/
